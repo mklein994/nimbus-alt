6a807044b6db04664c43faa87518f687685cb22e
Matthew Klein <mklein994@gmail.com>
2018-11-04 21:45:45 -0600

    Create main() and run() setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate failure;
+
+use failure::Error;
+
+type Result<T> = std::result::Result<T, Error>;
+
 fn main() {
+    if let Err(e) = run() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
     println!("Hello, world!");
+    Ok(())
 }
