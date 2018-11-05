d122d30cd275eceff50351a251da9f0db766d00e
Matthew Klein <mklein994@gmail.com>
2018-11-05 12:52:08 -0600

    Extract OWM weather url parsing to function

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 extern crate failure;
 extern crate reqwest;
+extern crate url;
 
 mod config;
 
 pub use self::config::Config;
 use failure::Error;
+use url::Url;
 
 pub fn run(config: &Config) -> Result<(), Error> {
     println!("{:?}", config);
-    let url = format!(
-        "http://api.openweathermap.org/data/2.5/weather?appid={}&id={}&units=metric",
-        config.api_key, config.location,
-    );
-    let res = reqwest::get(&url)?.error_for_status();
+
+    let url = owm_weather_url(&config)?;
+    let res = reqwest::get(url.as_str())?.error_for_status();
 
     match res {
         Ok(_) => println!("All good: {}", res?.text()?),
@@ -20,4 +20,12 @@ pub fn run(config: &Config) -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+fn owm_weather_url(config: &Config) -> Result<Url, url::ParseError> {
+    let url = format!(
+        "http://api.openweathermap.org/data/2.5/weather?appid={}&id={}&units=metric",
+        config.api_key, config.location,
+    );
+    Url::parse(&url)
 }
