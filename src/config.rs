ca4b667428770028d3d856fbebe54435d9e865b6
Matthew Klein <mklein994@gmail.com>
2018-11-05 14:18:03 -0600

    Enable lat/lon in config and get darksky weather

--- src/config.rs
+++ src/config.rs
@@ -5,8 +5,8 @@ pub struct Config {
     pub owm_api_key: String,
     pub darksky_api_key: String,
     pub location: String,
-    //pub latitude: f64,
-    //pub longitude: f64,
+    pub latitude: f64,
+    pub longitude: f64,
 }
 
 impl Config {
@@ -15,16 +15,16 @@ impl Config {
             .expect("OpenWeatherMap API key missing (NIMBUS_OWM_KEY).");
         let darksky_api_key = dotenv::var("NIMBUS_DARKSKY_KEY")
             .expect("Darksky API key missing (NIMBUS_DARKSKY_KEY).");
-        //let latitude = dotenv::var("NIMBUS_LATITUDE").expect("Missing latitude.");
-        //let longitude = dotenv::var("NIMBUS_LONGITUDE").expect("Missing longitude.");
+        let latitude = dotenv::var("NIMBUS_LATITUDE").expect("Missing latitude.");
+        let longitude = dotenv::var("NIMBUS_LONGITUDE").expect("Missing longitude.");
         let location = dotenv::var("NIMBUS_OWM_LOCATION").expect("Missing location.");
 
         Ok(Self {
             owm_api_key: owm_key,
             darksky_api_key,
             location,
-            //latitude: latitude.parse()?,
-            //longitude: longitude.parse()?,
+            latitude: latitude.parse()?,
+            longitude: longitude.parse()?,
         })
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,14 @@ pub fn run(config: &Config) -> Result<(), Error> {
         Err(e) => println!("error: {}", e),
     }
 
+    let url = darksky_weather_url(&config)?;
+    let res = reqwest::get(url.as_str())?.error_for_status();
+
+    match res {
+        Ok(_) => println!("All good: {}", res?.text()?),
+        Err(e) => println!("error: {}", e),
+    }
+
     Ok(())
 }
 
@@ -26,6 +34,14 @@ fn owm_weather_url(config: &Config) -> Result<Url, url::ParseError> {
     let url = format!(
         "http://api.openweathermap.org/data/2.5/weather?appid={}&id={}&units=metric",
         config.owm_api_key, config.location,
+    );
+    Url::parse(&url)
+}
+
+fn darksky_weather_url(config: &Config) -> Result<Url, url::ParseError> {
+    let url = format!(
+        "https://api.darksky.net/forecast/{}/{},{}",
+        config.darksky_api_key, config.latitude, config.longitude
     );
     Url::parse(&url)
 }
