90b5d70c35292ea569082f1122340c5438d71b44
Matthew Klein <mklein994@gmail.com>
2018-11-05 12:38:57 -0600

    Rearrange api key and location id in query string
    Preparing to make this easier to expand by just appending parameters.

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,8 @@ type Result<T> = std::result::Result<T, Error>;
 pub fn run(config: &Config) -> Result<()> {
     println!("{:?}", config);
     let url = format!(
-        "http://api.openweathermap.org/data/2.5/weather?id={}&units=metric&appid={}",
-        config.location, config.api_key,
+        "http://api.openweathermap.org/data/2.5/weather?appid={}&id={}&units=metric",
+        config.api_key, config.location,
     );
     let res = reqwest::get(&url)?.error_for_status();
 
